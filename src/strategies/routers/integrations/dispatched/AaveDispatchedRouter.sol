1b08b28d467c4c2d4fc94cf70a2580ebeb44522b
Maxence Raballand <maxenceraballand00@gmail.com>
2023-09-21 18:36:17 +0200

    advanced on AaveDispatchedRouter

--- src/strategies/routers/integrations/dispatched/AaveDispatchedRouter.sol
+++ src/strategies/routers/integrations/dispatched/AaveDispatchedRouter.sol
@@ -11,14 +11,21 @@ import {IERC20} from "mgv_src/IERC20.sol";
 /// @dev No tokens should be directly sent to this contract
 /// @dev This contract is to be used by the `Dispatcher` contract.
 contract AaveDispatchedRouter is MonoRouter, AaveMemoizer {
+  /// @notice Holds the storage key for this contract specific storage
   bytes32 internal immutable STORAGE_KEY;
 
+  /// @notice The default buffer size for Aave credit line
+  /// * 100 means we can redeem or borrow 100% of the credit line
+  /// * 0 means we can't redeem or borrow anything
   uint internal immutable DEFAULT_BUFFER_SIZE = 100;
 
+  /// @notice Storage Layout for `AaveDispatchedRouter`
   struct AaveDispatcherStorage {
-    mapping(address => mapping(IERC20 => uint)) buffer_size;
+    /// @notice The buffer size decrease for a given token and reserveId
+    mapping(address => mapping(IERC20 => uint)) buffer_size_decrease;
   }
 
+  /// @param storage_key The storage key for this contract specific storage
   constructor(uint routerGasreq_, address addressesProvider, uint interestRateMode, string memory storage_key)
     MonoRouter(routerGasreq_)
     AaveMemoizer(addressesProvider, interestRateMode)
@@ -26,6 +33,43 @@ contract AaveDispatchedRouter is MonoRouter, AaveMemoizer {
     STORAGE_KEY = keccak256(abi.encodePacked(storage_key));
   }
 
+  /// @notice Get the storage struct for this contract
+  /// @return s The storage struct
+  function getAaveDispatcherStorage() internal view returns (AaveDispatcherStorage storage s) {
+    bytes32 key = STORAGE_KEY;
+    assembly {
+      s.slot := key
+    }
+  }
+
+  /// @notice Sets the buffer size decrease for a given token and reserveId
+  /// @dev This is used to decrease the buffer size for a given token and reserveId
+  /// * This can only be called by the reserveId
+  /// @param token The token to set the buffer size decrease for
+  /// @param reserveId The reserveId to set the buffer size decrease for
+  /// @param amount The amount to decrease the buffer size by
+  function setBufferSizeDecrease(IERC20 token, address reserveId, uint amount) external onlyCaller(reserveId) {
+    require(amount <= 100, "AaveDispatchedRouter/InvalidAmount");
+    AaveDispatcherStorage storage s = getAaveDispatcherStorage();
+    s.buffer_size_decrease[reserveId][token] = amount;
+  }
+
+  /// @notice Gets the buffer size for a given token and reserveId
+  /// @dev This has to be called in order to get the maximum credit line that can be used by this contract
+  /// @param token The token to get the buffer size for
+  /// @param reserveId The reserveId to get the buffer size for
+  /// @return buffer_size The buffer size (or max credit line usage)
+  function getBufferSize(IERC20 token, address reserveId) internal view returns (uint buffer_size) {
+    AaveDispatcherStorage storage s = getAaveDispatcherStorage();
+    buffer_size = DEFAULT_BUFFER_SIZE;
+    uint decrease = s.buffer_size_decrease[reserveId][token];
+    if (decrease > 0) {
+      buffer_size -= decrease;
+    }
+  }
+
+  /// @dev Checks if user gave allowance for token and overlying
+  /// @inheritdoc	AbstractRouter
   function __checkList__(IERC20 token, address reserveId) internal view override {
     require(token.allowance(reserveId, address(this)) > 0, "AaveDispatchedRouter/NotApproved");
     Memoizer memory m;
@@ -54,5 +98,11 @@ contract AaveDispatchedRouter is MonoRouter, AaveMemoizer {
     return amount;
   }
 
-  function balanceOfReserve(IERC20 token, address reserveId) public view virtual override returns (uint) {}
+  ///@notice returns the amount of asset that the reserve has, either locally or on pool
+  ///@inheritdoc AbstractRouter
+  function balanceOfReserve(IERC20 token, address reserveId) public view virtual override returns (uint) {
+    Memoizer memory m;
+    setOwnerAddress(m, reserveId);
+    return overlyingBalanceOf(token, m) + balanceOf(token, m);
+  }
 }
