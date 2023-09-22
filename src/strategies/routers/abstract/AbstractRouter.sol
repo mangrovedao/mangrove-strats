9edbf836d580cf2e6200497dcb144e4c291c5035
Louis-Amas <amaslouis@protonmail.com>
2023-09-22 16:04:51 +0200

    feat(permit2): remove unused import

--- src/strategies/routers/abstract/AbstractRouter.sol
+++ src/strategies/routers/abstract/AbstractRouter.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.10;
 
 import {AccessControlled} from "mgv_strat_src/strategies/utils/AccessControlled.sol";
 import {IERC20} from "mgv_src/MgvLib.sol";
-import {ISignatureTransfer} from "lib/permit2/src/interfaces/ISignatureTransfer.sol";
 
 /// @title AbstractRouter
 /// @notice Partial implementation and requirements for liquidity routers.
