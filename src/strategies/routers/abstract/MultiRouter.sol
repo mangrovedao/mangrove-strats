9577e92fedae934d9b4d7e6cd6b057bf3a269548
lnist <16150076+lnist@users.noreply.github.com>
2023-09-25 14:08:25 +0200

    add natspec for MultiRouter

--- src/strategies/routers/abstract/MultiRouter.sol
+++ src/strategies/routers/abstract/MultiRouter.sol
@@ -4,8 +4,9 @@ pragma solidity ^0.8.18;
 import {IERC20} from "mgv_src/IERC20.sol";
 import {MonoRouter, AbstractRouter} from "./MonoRouter.sol";
 
-///@title `MultiRouter` instances have reserveId dependant sourcing strategies.
+///@title `MultiRouter` instances have token and reserveId dependant sourcing strategies.
 abstract contract MultiRouter is AbstractRouter {
+  ///@notice the specific router to use for a given token and reserveId.
   mapping(IERC20 token => mapping(address reserveId => MonoRouter)) public routes;
 
   ///@inheritdoc AbstractRouter
@@ -13,6 +14,10 @@ abstract contract MultiRouter is AbstractRouter {
     return routes[token][reserveId].ROUTER_GASREQ();
   }
 
+  ///@notice sets the router to use for a given token and reserve id.
+  ///@param token the token to set the router for.
+  ///@param reserveId the reserveId to set the router for.
+  ///@param router the router to set.
   function setRoute(IERC20 token, address reserveId, MonoRouter router) external onlyBound {
     routes[token][reserveId] = router;
   }
