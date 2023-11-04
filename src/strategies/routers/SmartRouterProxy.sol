08f1630f5dc613b11c93d2f8756d67c6d022a641
jkrivine <jean@mangrove.exchange>
2023-11-04 15:40:39 +0100

    remappings + missing imports

--- src/strategies/routers/SmartRouterProxy.sol
+++ src/strategies/routers/SmartRouterProxy.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier:	BSD-2-Clause
 pragma solidity ^0.8.18;
 
-import {Proxy} from "@openzepplin/contracts/proxy/proxy.sol";
+import {Proxy} from "@openzeppelin/contracts/proxy/proxy.sol";
+import {AccessControlled} from "@mgv-strats/src/strategies/utils/AccessControlled.sol";
+import {SmartRouter} from "./SmartRouter.sol";
 
 ///@title Mangrove Smart Router Proxy
 contract SmartRouterProxy is AccessControlled(msg.sender), Proxy {
@@ -11,7 +13,9 @@ contract SmartRouterProxy is AccessControlled(msg.sender), Proxy {
     IMPLEMENTATION = implementation;
   }
 
-  function _implementation() internal override returns (address) {
+  receive() external payable virtual {}
+
+  function _implementation() internal view override returns (address) {
     return address(IMPLEMENTATION);
   }
 }
