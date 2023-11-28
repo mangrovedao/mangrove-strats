506b3acb9d4d7c35f85d860b66866bc00d184d71
JeanKa <jean@mangrove.exchange>
2023-11-28 15:08:49 +0100

    router proxy no longer payable

--- src/strategies/routers/RouterProxy.sol
+++ src/strategies/routers/RouterProxy.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.8.18;
 
 import {AbstractRouter} from "./abstract/AbstractRouter.sol";
 
-/// @title Mangrove Router Proxy
+/// @title Minimalistic Mangrove Router Proxy
 /// @notice A proxy contract that delegates calls to an instance of an AbstractRouter contract.
 ///         It does not allow updates of implementation contract.
-/// @dev it assumes the IMPLEMENTATION uses first storage slot to hold the admin address
+/// @dev it assumes the IMPLEMENTATION uses first storage slot to hold the admin address (this is guaranteed as long as `AbstractRouter` is `AccessControlled`)
 contract RouterProxy {
   /// @notice The address of the deployed SmartRouter contract acting as the delegate implementation.
   /// @dev The SmartRouter instance must be AccessControlled to ensure the storage layout is matched.
@@ -18,18 +18,13 @@ contract RouterProxy {
   constructor(AbstractRouter implementation) {
     IMPLEMENTATION = implementation;
     // store the msg sender at address 0 (_admin storage slot on access controlled)
-    // We do not want to store the whole AccessControlled logic inside the proxy
     assembly {
       sstore(0, caller())
     }
   }
 
-  /// @notice Fallback function to receive ETH.
-  /// @dev This function is marked as virtual so it can be overridden by inheriting contracts if required.
-  receive() external payable virtual {}
-
   /// @notice Fallback function to delegate calls to the implementation contract.
-  fallback() external payable {
+  fallback() external {
     AbstractRouter implementation = IMPLEMENTATION;
     assembly {
       // Copy msg.data. We take full control of memory in this inline assembly
