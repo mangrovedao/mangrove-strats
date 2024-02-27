10ee4de5b88b4e4953cbe7b6b1a4a9e3b4d04aaf
Maxence Raballand <dev@maxencerb.com>
2024-02-27 18:06:39 +0100

    remove scripts

--- verify.sh
+++
@@ -1,4 +0,0 @@
-forge verify-contract 0x46D3CF10AAb522E757005AADd911BC6331b79bFb src/strategies/routing_logic/orbit/OrbitLogicStorage.sol:OrbitLogicStorage \
-  --verifier-url 'https://api.routescan.io/v2/network/testnet/evm/168587773/etherscan' \
-  --etherscan-api-key "verifyContract" \
-  --constructor-args $(cast abi-encode "constructor(address)" 0xac841600ea0FfE66CEbbFc601D60783D8fb54B94)
