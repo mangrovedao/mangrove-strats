b3e5f1706b83dfe0edad6574afedad0411dbd41c
Solthodox <xabierblockchain@gmail.com>
2025-03-12 11:18:06 +0100

    fix:wrong argument in event emission

--- src/strategies/offer_maker/market_making/kandel/ERC4626KandelSeeder.sol
+++ src/strategies/offer_maker/market_making/kandel/ERC4626KandelSeeder.sol
@@ -58,9 +58,7 @@ contract ERC4626KandelSeeder is AbstractKandelSeeder {
     router.bind(address(kandel));
     // Set the Kandel as router admin
     router.setAdmin(address(kandel));
-    emit NewERC4626Kandel(
-      address(kandel), olKeyBaseQuote.hash(), olKeyBaseQuote.flipped().hash(), address(kandel), owner
-    );
+    emit NewERC4626Kandel(owner, olKeyBaseQuote.hash(), olKeyBaseQuote.flipped().hash(), address(kandel), owner);
   }
   ///@notice Deploys a new instance of ERC4626Router.
   ///@return The address of the newly deployed ERC4626Router.
